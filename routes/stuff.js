920ccbf20c09a8c351950160f01fdc21fe1f4825
S-Syrichko <s-syrichko@protonmail.com>
2023-07-08 21:22:54 +0200

    Complete backend

---
+++ routes/stuff.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer-config");
+
+
+const stuffCtrl = require("../controllers/stuff");
+
+router.post("/", auth, multer, stuffCtrl.createThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.get("/", auth, stuffCtrl.getAllThings);
+router.put("/:id", auth, multer, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+
+module.exports = router;
